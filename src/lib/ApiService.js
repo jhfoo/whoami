e8377912fb2b863c625eb12a369276f284ef2bec
Foo JH <foojihaw@hotmail.com>
2022-04-21 23:15:58 -0500

    NEW: complete remote HTTP request response

--- src/lib/ApiService.js
+++ src/lib/ApiService.js
@@ -5,12 +5,25 @@ module.exports = {
   mixins: [ApiService],
   settings: {
     port: 8880,
+    routes: [{
+      aliases: {
+        'GET whoami'(req, res) {
+          const HeaderStr = Object.keys(req.headers).map((key) => {
+            return `${key}: ${req.headers[key]}`
+          }).join('\n')
+          res.end(`Remote address: ${req.socket.remoteAddress}
+Remote port: ${req.socket.remotePort}
+${HeaderStr}`)
+        },
+      }
+    }]
   },
   actions: {
-    info: whoamiInfo,
+    // info: whoamiInfo,
   }
 }
 
 function whoamiInfo(ctx) {
+  console.log(ctx.transporter)
   return 'ok'
 }
